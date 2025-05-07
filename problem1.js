9d436e61a82820a08f86d0dd8a0145c9f6278365
agneshagata <agneshagatapanjaitan@gmail.com>
2025-05-08 00:27:24 +0700

    Add files via upload

---
+++ problem1.js
@@ -0,0 +1,8 @@
+let firstName = 'John';
+
+let lastName = 'Doe';
+
+let bornyear = '2014' - '1998';
+
+let message = "Halo, " + firstName + " " + lastName + ". Kamu berusia " + bornyear +" " + "tahun.";
+console.log(message);
---
+++ problem2.js
@@ -0,0 +1,6 @@
+let r = 4;
+let T = 20;
+
+let luas = 2 * 3.14 * r * (r + T);
+
+console.log("Luas permukaan tabung = " + luas.toFixed(2));
